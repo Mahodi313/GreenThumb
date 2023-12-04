c8240272095206454d59ce3a1dc6cc253a2f36bc
Mahodi313 <Mehdiali15@outlook.com>
2023-12-04 17:27:42 +0100

    SeededInstructionsComplete

--- GreenThumb/Data/GreenThumbDbContext.cs
+++ GreenThumb/Data/GreenThumbDbContext.cs
@@ -26,6 +26,10 @@ namespace GreenThumb.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            // Seeding
+
+            //Plants
+
             modelBuilder.Entity<PlantModel>().HasData(
             new PlantModel()
             {
@@ -120,6 +124,137 @@ namespace GreenThumb.Data
                 Name = "Arctic Willow",
                 Description = "Arctic Willow, a whispering ballerina of the tundra, graces the icy landscape with its delicate, silvery branches. Amidst the frozen expanse, it stands as a testament to nature's ability to find grace and beauty in the harshest climates."
             });
+
+            // Instructions
+
+            modelBuilder.Entity<InstructionModel>().HasData(
+                new InstructionModel() 
+                {
+                    InstructionId = 1,
+                    Name = "Watering",
+                    Description = "Deep and infrequent watering rather than frequent shallow watering. Water when the top inch of soil feels dry.",
+                    PlantId = 1
+                },
+
+                new InstructionModel() 
+                {
+                    InstructionId = 2,
+                    Name = "Pruning",
+                    Description = "Prune in late winter or early spring to remove dead or diseased wood and shape the plant.",
+                    PlantId= 1
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 3,
+                    Name = "Container",
+                    Description = "Ensure the pot has drainage holes to prevent waterlogged soil.",
+                    PlantId = 2
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 4,
+                    Name = "Choose the Right Location",
+                    Description = "Select a location with well-draining soil and adequate sunlight.",
+                    PlantId = 3
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 5,
+                    Name = "Spacing",
+                    Description = "Ensure proper spacing between maple trees to allow for healthy growth.",
+                    PlantId = 3
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 6,
+                    Name = "Temperature",
+                    Description = "They prefer warm temperatures. Protect them from cold drafts and frost, especially during winter.",
+                    PlantId = 4
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 7,
+                    Name = "Soil",
+                    Description = "They prefer well-draining soil. Amend heavy or compacted soil with organic matter to improve drainage.",
+                    PlantId = 5
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 8,
+                    Name = "Fertilization",
+                    Description = "Use a balanced, all-purpose fertilizer when planting. Additional fertilization may not be necessary if the soil is nutrient-rich.",
+                    PlantId = 5
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 9,
+                    Name = "Barriers",
+                    Description = "Many bamboo varieties are spreading and can become invasive. Use barriers (rhizome barriers) to contain the spread of the roots.",
+                    PlantId = 6
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 10,
+                    Name = "Mulching",
+                    Description = "Mulch around the base of the palm tree to conserve moisture, regulate soil temperature, and suppress weeds.",
+                    PlantId = 7
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 11,
+                    Name = "Good Air Circulation",
+                    Description = "Ensure proper air circulation to minimize the risk of fungal diseases.",
+                    PlantId = 8
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 12,
+                    Name = "Humidity",
+                    Description = "Orchids often require higher humidity levels. In drier climates, use humidity trays, humidifiers, or group plants together to create a more humid microenvironment.",
+                    PlantId = 9
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 13,
+                    Name = "Propagating",
+                    Description = "Succulents can be propagated from leaf or stem cuttings. Allow cuttings to dry and callus before planting.",
+                    PlantId = 10
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 14,
+                    Name = "Gravel",
+                    Description = "Put it around the base of lavender to conserve soil moisture and suppress weeds.",
+                    PlantId = 11
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 15,
+                    Name = "Planting Time",
+                    Description = "Plant tulip bulbs in the fall before the first frost. This allows them to establish roots before winter.",
+                    PlantId = 12
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 16,
+                    Name = "Indirect Light",
+                    Description = "Ferns thrive in indirect or filtered light. Avoid exposing them to direct sunlight, especially during the hot afternoon hours.",
+                    PlantId = 13
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 17,
+                    Name = "Wiring",
+                    Description = "Wire branches carefully to shape and train your bonsai. Be mindful of the tree's natural growth pattern, and remove wires before they cut into the bark.",
+                    PlantId = 14
+                },
+                new InstructionModel() 
+                {
+                    InstructionId = 18,
+                    Name = "Natural Shape",
+                    Description = "Allow Arctic Willow to maintain its natural, arching form. Minimal pruning is needed to enhance its graceful appearance. Embrace its natural growth habit for a more authentic look.",
+                    PlantId = 15
+                });
         }
     }
 }
---
+++ GreenThumb/Migrations/20231204162648_SeedInstructions.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenThumb.Migrations
+{
+    public partial class SeedInstructions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Instruction",
+                columns: new[] { "instruction_id", "description", "name", "plant_id" },
+                values: new object[,]
+                {
+                    { 1, "Deep and infrequent watering rather than frequent shallow watering. Water when the top inch of soil feels dry.", "Watering", 1 },
+                    { 2, "Prune in late winter or early spring to remove dead or diseased wood and shape the plant.", "Pruning", 1 },
+                    { 3, "Ensure the pot has drainage holes to prevent waterlogged soil.", "Container", 2 },
+                    { 4, "Select a location with well-draining soil and adequate sunlight.", "Choose the Right Location", 3 },
+                    { 5, "Ensure proper spacing between maple trees to allow for healthy growth.", "Spacing", 3 },
+                    { 6, "They prefer warm temperatures. Protect them from cold drafts and frost, especially during winter.", "Temperature", 4 },
+                    { 7, "They prefer well-draining soil. Amend heavy or compacted soil with organic matter to improve drainage.", "Soil", 5 },
+                    { 8, "Use a balanced, all-purpose fertilizer when planting. Additional fertilization may not be necessary if the soil is nutrient-rich.", "Fertilization", 5 },
+                    { 9, "Many bamboo varieties are spreading and can become invasive. Use barriers (rhizome barriers) to contain the spread of the roots.", "Barriers", 6 },
+                    { 10, "Mulch around the base of the palm tree to conserve moisture, regulate soil temperature, and suppress weeds.", "Mulching", 7 },
+                    { 11, "Ensure proper air circulation to minimize the risk of fungal diseases.", "Good Air Circulation", 8 },
+                    { 12, "Orchids often require higher humidity levels. In drier climates, use humidity trays, humidifiers, or group plants together to create a more humid microenvironment.", "Humidity", 9 },
+                    { 13, "Succulents can be propagated from leaf or stem cuttings. Allow cuttings to dry and callus before planting.", "Propagating", 10 },
+                    { 14, "Put it around the base of lavender to conserve soil moisture and suppress weeds.", "Gravel", 11 },
+                    { 15, "Plant tulip bulbs in the fall before the first frost. This allows them to establish roots before winter.", "Planting Time", 12 },
+                    { 16, "Ferns thrive in indirect or filtered light. Avoid exposing them to direct sunlight, especially during the hot afternoon hours.", "Indirect Light", 13 },
+                    { 17, "Wire branches carefully to shape and train your bonsai. Be mindful of the tree's natural growth pattern, and remove wires before they cut into the bark.", "Wiring", 14 },
+                    { 18, "Allow Arctic Willow to maintain its natural, arching form. Minimal pruning is needed to enhance its graceful appearance. Embrace its natural growth habit for a more authentic look.", "Natural Shape", 15 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Instruction",
+                keyColumn: "instruction_id",
+                keyValue: 18);
+        }
+    }
+}
