47133a31262b97c6c1f3211fcc02027c5bba4d05
Mahodi313 <Mehdiali15@outlook.com>
2023-12-04 13:54:21 +0100

    DbContextAdded

---
+++ GreenThumb/Data/GreenThumbDbContext.cs
@@ -0,0 +1,25 @@
+﻿using GreenThumb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreenThumb.Data
+{
+    public class GreenThumbDbContext : DbContext
+    {
+        public GreenThumbDbContext()
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<GardenModel> Gardens { get; set; }
+        public DbSet<PlantModel> Plants { get; set; }
+        public DbSet<InstructionModel> Instruction { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=GreenThumbDb;Trusted_Connection=True;");
+        }
+    }
+}
