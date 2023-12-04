f44c7cc77f16147cb7fc12f5cb8db31f2655b01f
Mahodi313 <Mehdiali15@outlook.com>
2023-12-04 19:15:16 +0100

    GenericRepoAdded

---
+++ GreenThumb/Data/GreenRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenThumb.Data
+{
+    public class GreenRepository<T> where T : class
+    {
+        private readonly GreenThumbDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GreenRepository(GreenThumbDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<List<T>> GetAll() 
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T?> GetById(int id) 
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task Add(T entity) 
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity) 
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
