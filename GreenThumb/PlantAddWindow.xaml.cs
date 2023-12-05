a809d230ebea248a5bc59715f8a179ab0c0d9d2b
Mahodi313 <Mehdiali15@outlook.com>
2023-12-05 18:04:09 +0100

    AddPlantWindowDesignComplete

---
+++ GreenThumb/PlantAddWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GreenThumb
+{
+    /// <summary>
+    /// Interaction logic for PlantAddWindow.xaml
+    /// </summary>
+    public partial class PlantAddWindow : Window
+    {
+        public PlantAddWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddPlant_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddInstruction_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
