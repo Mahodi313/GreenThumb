d6eaeffad3ec3bf6e4d2799b649bc3bca86a0117
Mahodi313 <Mehdiali15@outlook.com>
2023-12-05 22:53:34 +0100

    LaptopInitialCreate

---
+++ GreenThumb/Migrations/20231205214740_InitialCreateLaptop.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenThumb.Migrations
+{
+    public partial class InitialCreateLaptop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
