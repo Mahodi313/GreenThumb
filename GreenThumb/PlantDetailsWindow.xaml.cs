4cac3c4714725c3fbdd43a10c008ef6a7076a00d
Mahodi313 <Mehdiali15@outlook.com>
2023-12-05 11:26:02 +0100

    PlantDetailsDesignFinish

---
+++ GreenThumb/PlantDetailsWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using GreenThumb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GreenThumb
+{
+    /// <summary>
+    /// Interaction logic for PlantDetailsWindow.xaml
+    /// </summary>
+    public partial class PlantDetailsWindow : Window
+    {
+        public PlantDetailsWindow(PlantModel plant, UserModel user)
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
